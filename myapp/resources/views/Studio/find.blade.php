246a194729c403a1e01d8cbbf3bfda2993fcb7ba
sigel-i <rock.heavy.gt@gmail.com>
2020-09-29 17:17:35 +0900

    roomsにNopを追加

--- myapp/resources/views/Studio/find.blade.php
+++ myapp/resources/views/Studio/find.blade.php
@@ -5,15 +5,11 @@
   <form action="/studio/find" method="post">
   @csrf
   <input type="text" name="input" value="{{$input}}">
-  <input type="submit" value="find"><br>
+  <input type="submit" value="search"><br>
   </form>
   @if (isset($studio))
-  <table>
-  <tr><th>Data</th></tr>
-  <tr>
-    <td>{{$studio->getData()}}</td>
-  </tr>
-  </table>
+  <a href="/studio">
+
   @endif
 @endsection
 
