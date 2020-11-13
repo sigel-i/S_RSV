aa7cf887f0f40ba25ef7a5396550ed3390aa59b2
sigel-i <rock.heavy.gt@gmail.com>
2020-11-13 14:25:18 +0900

    renamefiles

--- myapp/app/csvimport.php
+++ myapp/app/csvimport.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class CSVimport extends Model
+class csvimport extends Model
 {
     protected $fillable = ['name','email','pref','city','tel','url','image_url'];
 }
--- myapp/database/seeds/DatabaseSeeder.php
+++ myapp/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(csvimportsTableSeeder::class);
+        $this->call(StudioTableSeeder::class);
     }
 }
