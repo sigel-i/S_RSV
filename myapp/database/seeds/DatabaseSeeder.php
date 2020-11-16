b9abf0bd954ea820fe20edd6d1ff2228d3d1699e
sigel-i <rock.heavy.gt@gmail.com>
2020-11-16 17:56:45 +0900

    DatabaseSeeder修正

--- myapp/database/seeds/DatabaseSeeder.php
+++ myapp/database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(StudioTableSeeder::class);
+        $this->call([StudioTableSeeder::class,
+                    RoomTableSeeder::class,
+                    UserTableSeeder::class,
+                    CommentTableSeeder::class,
+        ]);
     }
 }
