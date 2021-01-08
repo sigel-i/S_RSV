53d2f3e51e084281831e97f58bb8568560c78c90
sigel-i <rock.heavy.gt@gmail.com>
2021-01-08 15:47:56 +0900

    タイトル変更

--- myapp/resources/views/layouts/app.blade.php
+++ myapp/resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
   <meta name="description" content="バンドマン 音楽スタジオ 検索サイト">
   <link rel='stylesheet' href='https://unpkg.com/ress/dist/ress.min.css'>
   <title>
-    @yield('title') | 音楽スタジオ
+    @yield('title') | 音楽スタジオ検索
   </title>
   <!-- Font Awesome -->
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
