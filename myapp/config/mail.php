8ad5da133fe55ba8daa3052f66bab2922c2b68ed
sigel-i <rock.heavy.gt@gmail.com>
2021-01-14 17:55:01 +0900

    gmailに変更

--- myapp/config/mail.php
+++ myapp/config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.sendgrid.net'),
+            'host' => env('MAIL_HOST', 'smtp.gmail.com'),
             'port' => env('MAIL_PORT', 587),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
             'username' => env('MAIL_USERNAME','directors.ishii@gmail.com'),
-            'password' => env('MAIL_PASSWORD','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'),
+            'password' => env('MAIL_PASSWORD','extvfsmelexowdwc'),
             'timeout' => null,
             'auth_mode' => null,
         ],
