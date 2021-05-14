76e1d4d5c5269a7e1e0bae991ad02bd711f60e05
sigel-i <rock.heavy.gt@gmail.com>
2021-05-14 15:38:19 +0900

    TOP変更

--- myapp/resources/views/Studio/search.blade.php
+++ myapp/resources/views/Studio/search.blade.php
@@ -5,11 +5,10 @@
 <div class="search-area">
   <form action="/studio" method="get">
     @csrf
-    <title>{{ config('app.name') }}</title>
-    <div class="wrap">
+     <div class="wrap">
         <div class="overlay">
-        <div class="search-title">Studio Search<br><h1>東京都内 音楽スタジオ検索</h1></div><br>
-
+            <div class="search-title">Studio Search<br><h1>東京都内 音楽スタジオ検索</h1></div>
+            <br>
             <div class="center">
               <label2 for="city">エリアを選択する</label>
               <select name="city" id="city" class="custom-select sources">
@@ -30,7 +29,7 @@
               <input class="btn-flat-border" type="submit" value="検索">
             </div>
           </div>
-    </div>
+     </div>
   </form>
 </div>
 @endsection
