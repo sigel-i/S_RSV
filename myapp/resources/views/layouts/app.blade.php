903b0d3587fb763081c22430cb887d8ddfb292e1
sigel-i <rock.heavy.gt@gmail.com>
2023-05-30 17:25:54 +0900

    app.blade.php編集

--- myapp/resources/views/layouts/app.blade.php
+++ myapp/resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
 
     gtag('config', 'G-6DXVGS8982');
   </script>
+
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
